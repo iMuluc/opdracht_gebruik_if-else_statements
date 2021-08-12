5b95f404646f83414808cee99cbbc228dc3e3f2e
iMuluc <iMertUluc@gmail.com>
2021-08-12 13:02:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const age = 21;
+if (age > 18) {
+    console.log("U mag naar binnen")
+} else {
+    console.log("U wordt vriendelijk verzocht buiten te blijven")
+}
+
+const isFemale = true;
+if (isFemale == true) {
+    console.log("Je bent een vrouw")
+} else {
+    console.log("Je bent een man")
+}
+
+const driverStatus = 'bob';
+if (driverStatus == 'bob') {
+    console.log("U bent bob en mag rijden!")
+} else {
+    console.log("U bent niet bob en mag niet rijden!")
+}
